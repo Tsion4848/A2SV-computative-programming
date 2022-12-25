b33af578c4c367f6d5aabae9a083000c89f9129f
Tsion Moges <tsionmoges25@gmail.com>
2022-12-25 03:34:36 +0300

    Time: 26 ms (97.50%), Space: 13.9 MB (56.23%) - LeetHub

--- 1678-goal-parser-interpretation/1678-goal-parser-interpretation.py
+++ 1678-goal-parser-interpretation/1678-goal-parser-interpretation.py
@@ -1,18 +1,20 @@
 class Solution:
     def interpret(self, command: str) -> str:
-        answer = ""
+#         answer = ""
         
-        command = [*command]
-        print(command)
+#         command = [*command]
+#         print(command)
         
-        for i in range(len(command)):
-            if command[i] == 'G':
-                answer += "G"
-            if command[i] == '(':
-                if command[i+1] == ')':
-                    answer += "o"
-            if command[i] == ')':
-                answer += ""
-            if command[i] == '(' and command[i+1] == 'a' and command[i+2] == 'l' and command[i+3] == ')':
-                answer += "al"
-        return answer
+#         for i in range(len(command)):
+#             if command[i] == 'G':
+#                 answer += "G"
+#             if command[i] == '(':
+#                 if command[i+1] == ')':
+#                     answer += "o"
+#             if command[i] == ')':
+#                 answer += ""
+#             if command[i] == '(' and command[i+1] == 'a' and command[i+2] == 'l' and command[i+3] == ')':
+#                 answer += "al"
+        # return answer
+        return command.replace("()", "o").replace("(al)", "al")
+            
