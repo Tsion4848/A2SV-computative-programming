9404d9c432f7a268d2042849dbaba6bc858ad66b
Tsion Moges <tsionmoges25@gmail.com>
2022-12-26 18:37:03 +0300

    Create python_power.py

---
+++ python_power.py
@@ -0,0 +1,10 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+# num = []
+# for i in range(3):
+#     num.append(int(input()))
+a = int(input())
+b = int(input())
+m = int(input())
+
+print(pow(a,b))
+print(pow(a,b,m))
