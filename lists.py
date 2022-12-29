47d3edbb0c60802b32e7e01c5125ea073651479d
Tsion Moges <tsionmoges25@gmail.com>
2022-12-29 04:34:18 +0300

    Update lists.py

--- lists.py
+++ lists.py
@@ -1,27 +1,24 @@
 if __name__ == '__main__':
-    N = int(input())
-    inputs = []
-    splitted = []
+    n = int(input())
+    arr = []
     answer = []
-    for i in range(N):
-        inputs.append((input()))
-        
-    for i in inputs:
-        splitted.append(i.split())
-        
-    for i in range(len(splitted)):
-        if (splitted[i][0] == 'insert'):
-            answer.insert(int(splitted[i][1]),int(splitted[i][2]))
-        elif (splitted[i][0] == 'remove'):
-            answer.remove(int(splitted[i][1]))            
-        elif (splitted[i][0] == 'append'):
-            answer.append(int(splitted[i][1]))
-        elif (splitted[i][0] == 'print'):
+    for i in range(n):
+        arr.append(input().split())
+    # print(arr)
+
+    for i in range(len(arr)):
+        if (arr[i][0] == 'insert'):
+            answer.insert(int(arr[i][1]),(arr[i][2]))
+        elif (arr[i][0] == 'remove'):
+            answer.remove(int(arr[i][1]))           
+        elif (arr[i][0] == 'append'):
+            answer.append(arr[i][1])
+        elif (arr[i][0] == 'print'):
             print(answer)
-        elif (splitted[i][0] == 'sort'):
+        elif (arr[i][0] == 'sort'):
             answer.sort()
-        elif (splitted[i][0] == 'pop'):
+        elif (arr[i][0] == 'pop'):
             answer.pop()
-        elif (splitted[i][0] == 'reverse'):
+        elif (arr[i][0] == 'reverse'):
             answer.reverse()
-        # answer = [int(j) for j in answer]
+        answer = [int(j) for j in answer]
