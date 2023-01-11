7269607d52c116ab4c240cdb3eecc8492ca4de24
Tsion Moges <tsionmoges25@gmail.com>
2023-01-11 13:53:15 +0300

    Time: 378 ms (5.10%), Space: 14.2 MB (8.01%) - LeetHub

---
+++ 0811-subdomain-visit-count/0811-subdomain-visit-count.py
@@ -0,0 +1,18 @@
+class Solution:
+    from collections import Counter
+    def subdomainVisits(self, cpdomains: List[str]) -> List[str]:
+        res = []
+        count = Counter()
+        
+        for cpdom in cpdomains:
+            num, dom = cpdom.split()
+            num, dom = int(num), dom.split('.')
+            
+            print(num)
+            print(dom)
+            
+            for i in reversed(range(len(dom))):
+                count['.'.join(dom[i:])] += num
+                print(count)
+                
+        return [str(amount) + ' ' + domain for domain, amount in count.items()]
